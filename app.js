90a63357160df4dc08ae9091f087a28294f24fd7
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-30 00:17:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+function convertToCamelCase(text){
+  let arr = Array.from(text)
+  for(i = 0 ; i < arr.length ; i++){
+    if(arr[i] == `-`){
+      arr.splice(i,1)
+      arr[i] = arr[i].toUpperCase()
+    }
+  }
+  return arr.join(``)
+}
+const dashedText = "background-color";
+console.log(convertToCamelCase(dashedText)); // "backgroundColor"
